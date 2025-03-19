662463b55691c6c0e5b20cd6d5e239e3d10ded44
KerissaGit <kerissa525@gmail.com>
2025-03-19 13:56:57 -0400

    Passes all tests

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,110 @@
 class Author:
-    pass
+    all_authors = []
 
+    def __init__(self, name):
+        self.name = name
+        Author.all_authors.append(self)
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name (self, value):
+        if not isinstance(value, str):
+            raise TypeError("Name must be a string")
+        self._name = value
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
+    
+    def books(self):
+        return [contract.book for contract in self.contracts()]
+    
+    def sign_contract(self, book, date, royalties):
+        contract = Contract(self, book, date, royalties)
+        return contract
+    
+    def total_royalties(self):
+        return sum(contract.royalties for contract in self.contracts())
+        
 
 class Book:
-    pass
+    all_books = []
+
+    def __init__(self, title):
+        self._title = title
+        Book.all_books.append(self)
+
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title (self, value):
+            if not isinstance(value, str):
+                raise TypeError("title must be a string")
+            self._title = value
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.book == self]
+    
+    def authors(self):
+        return [contract.author for contract in self.contracts()]
 
 
 class Contract:
-    pass
+    all = []
+
+    def __init__(self, author, book, date, royalties):
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+        Contract.all.append(self)
+
+    @property
+    def author(self):
+        return self._author
+    
+    @author.setter
+    def author(self, value):
+        if not isinstance(value, Author):
+            raise TypeError("Author must be an instance of Author class")
+        self._author = value
+    
+    @property
+    def book(self):
+        return self._book
+    
+    @book.setter
+    def book(self, value):
+        if not isinstance(value, Book):
+            raise TypeError("Book must be an instance of Book class")
+        self._book = value
+    
+    @property
+    def date(self):
+        return self._date
+    
+    @date.setter
+    def date(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Date must be a string")
+        self._date = value
+    
+    @property
+    def royalties(self):
+        return self._royalties
+    
+    @royalties.setter
+    def royalties(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Royalties must be an interger")
+        self._royalties = value
+    
+    @classmethod
+    def contracts_by_date(cls, date):
+        return [contract for contract in Contract.all if contract.date == date]
+    
+
